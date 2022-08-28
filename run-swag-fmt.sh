79986455618d397608470fd772f1dc0cca7c521e
Fred Muya <kingkonig@gmail.com>
2022-08-28 17:39:40 +0300

    Configure swag fmt

---
+++ run-swag-fmt.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if ! which swag &> /dev/null
+then
+    echo "swag could not be found. Please ensure it is installed and available on your PATH."
+    exit 1
+fi
+
+swag fmt $@
